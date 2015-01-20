2fbd9bc3f21eb9fa5c8834e9c85f68a032bee4dc
SeaOfLee <l.richardson1@gmail.com>
2015-01-20 13:48:59 -0800

    Edit JS and CSS style, game works

--- rps.js
+++ rps.js
@@ -1,80 +1,135 @@
-// I started here with a user prompt and wrote the game logic at the bottom first. 
-// The game worked using the prompt below.
 
 // var myChoice = prompt("Do ya feel lucky, punk? Please choose Rock, Paper or Scissors.");
 // myChoice = myChoice.toLowerCase();
 // console.log(myChoice);
 
-//Trying to use an event handler so clicking on text "Rock" on web page defines the myChoice variable. 
-// Loading the HTML file and clicking "Rock", "Paper" or "Scissors" triggers the correct console log.
-
 var choseRock = document.getElementById("rock");
-	choseRock.onclick = function() {
-		console.log("user chose rock");
-		myChoice = "rock";
-	}
-
 var chosePaper = document.getElementById("paper");
-	chosePaper.onclick = function() {
-		console.log("user chose paper");
-		myChoice = "paper";
-	}
-
 var choseScissors = document.getElementById("scissors");
-	choseScissors.onclick = function() {
-		console.log("user chose scissors");
-		myChoice = "scissors";
-	}		
+	
+choseRock.onclick = function() {
+	console.log("user chose rock");
+	myChoice = "rock";	
+	determineWinner(myChoice, generateCompChoice());
+};
 
+chosePaper.onclick = function() {
+	console.log("user chose paper");
+	myChoice = "paper";
+	determineWinner(myChoice, generateCompChoice());
+};
 
-// Defines computer's choice of "rock", "paper" or "scissors"
-var computerChoice = Math.random();
+choseScissors.onclick = function() {
+	console.log("user chose scissors");
+	myChoice = "scissors";
+	determineWinner(myChoice, generateCompChoice());
+};
 
-if(computerChoice < 0.34) {
-	computerChoice = "rock";
-}
-else if (computerChoice < 0.67) {
-	computerChoice = "paper";
-}
-else {
-	computerChoice = "scissors";
-}
+// Defines computer's choice of "rock", "paper" or "scissors". 
+// Math.floor(Math.ramdom() * 3) gives us a nice whole number, either 0,
+// 1 or 2. Easier to read, avoids rounding errors. 
 
-console.log(computerChoice);
-
-
-//The game logic determines a winner by comparing myChoice to computerChoice
-if (myChoice == "rock") {
-	if(computerChoice == "rock") {
-		alert("You both chose rock. Tie!");
-	}
-	else if (computerChoice == "paper") {
-		alert("Paper beats rock. You lose!");
-	}
-	else {
-		alert("Rock beats scissors. You win!");
-	}
+function generateCompChoice() {
+	var choiceNumber = Math.floor(Math.random() * 3);
+		if(choiceNumber === 0) {
+			return "rock";
+		}
+		else if (choiceNumber === 1) {
+			return "paper";
+		}
+		else {
+			return "scissors";
+		}
 }	
-else if(myChoice == "paper") {
-	if(computerChoice == "rock") {
-		alert("Paper beats rock. You win, winner!");
-	}	
-	else if(computerChoice == "paper") {
-		alert("You both chose Paper. Tie.");
-	}
-	else {
-		alert("Scissors beats paper. You lose.");
-	}	
+
+var resultsText = document.getElementById('results');
+// var loseText = document.getElementById('results');
+
+function alertWinner() {
+	resultsText.innerHTML = "You Win! :-)";
+	resultsText.style.color="red";
+}
+
+function alertLoser() {	
+	resultsText.innerHTML = "You Lose :-(";
+	resultsText.style.color="red";
 }	
-else {
-	if(computerChoice == "rock") {
-		alert("Rock beats scissors. You lose!");
+
+function alertTie() {
+	resultsText.innerHTML = "Both chose " + myChoice + ". Tie!";
+	resultsText.style.color="red";
+}
+
+//The game logic determines a winner by comparing playerChoice to compChoice.
+// An alert is triggered, changing the color and HTML of the result message.
+
+function determineWinner(playerChoice, compChoice) {
+	console.log('playerChoice', playerChoice);
+	console.log('compChoice', compChoice);
+	if (playerChoice === compChoice) {
+		alertTie();
+		alert("Both chose " + playerChoice + ". Tie!");
+		resultsText.style.color="black";
 	}
-	else if(computerChoice == "paper") {
-		alert("Scissors beats paper. You win!");
+	if (playerChoice === "rock" && compChoice === "scissors") {
+		alertWinner();
+		alert("Computer chose scissors. You win!");
+		resultsText.style.color="black";
 	}
-	else {
-		alert("Scissors sciss each other. Tie.")
+	if (playerChoice === "paper" && compChoice === "rock") {
+		alertWinner();
+		alert("Computer chose rock. You win!");
+		resultsText.style.color="black";
+	}
+	if (playerChoice === "scissors" && compChoice === "paper") {
+		alertWinner();
+		alert("Computer chose paper. You win.");
+		resultsText.style.color="black";
+	}
+	if (compChoice === "rock" && playerChoice === "scissors") {
+		alertLoser();
+		alert("Computer chose Rock. You Lose.");
+		resultsText.style.color="black";
+	}
+	if (compChoice === "paper" && playerChoice === "rock") {
+		alertLoser();
+		alert("Computer chose Paper. You lose.");
+		resultsText.style.color="black";
+	}
+	if (compChoice === "scissors" && playerChoice === "paper") {
+		alertLoser();
+		alert("Computer chose Scissors. You lose.");
+		resultsText.style.color="black";
 	}
 }
 
+// 	if (myChoice == "rock") {
+// 		if(computerChoice == "rock") {
+// 			alert("You both chose rock. Tie!");
+// 		}
+// 		else if (computerChoice == "paper") {
+// 			alert("Paper beats rock. You lose!");
+// 		} else {
+// 			alert("Rock beats scissors. You win!");
+// 		}
+// 	}	
+// 	else if(myChoice == "paper") {
+// 		if(computerChoice == "rock") {
+// 			alert("Paper beats rock. You win, winner!");
+// 		}	
+// 		else if(computerChoice == "paper") {
+// 			alert("You both chose Paper. Tie.");
+// 		} else {
+// 			alert("Scissors beats paper. You lose.");
+// 		}	
+// 	}	
+// 	else {
+// 		if(computerChoice == "rock") {
+// 			alert("Rock beats scissors. You lose!");
+// 		}
+// 		else if(computerChoice == "paper") {
+// 			alert("Scissors beats paper. You win!");
+// 		} else {
+// 			alert("Scissors sciss each other. Tie.");
+// 		}
+// 	}
